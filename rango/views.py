a6646da10f0f29cc1b968fd7c2bdf8245da9b992
Daniel Hally <2660097h@student.gla.ac.uk>
2023-01-22 17:54:45 +0000

    Ch4: templates, static, media

--- rango/views.py
+++ rango/views.py
@@ -3,10 +3,9 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("""
-        <p>Rango says hey there partner!</p>
-        <p><a href="/rango/about/">About</a></p>
-    """)
+    context_dict = {'boldmessage' : 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
