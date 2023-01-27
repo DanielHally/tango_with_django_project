6514941f53f9aa678208d3327c3e6099fd0fd95f
DanielHally <2660097h@student.gla.ac.uk>
2023-01-27 12:28:00 +0000

    Ch6: slug, category pages

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render
 
+from rango.models import Category, Page
+
 
 def index(request):
-    context_dict = {'boldmessage' : 'Crunchy, creamy, cookie, candy, cupcake!'}
-
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {
+        'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
+        'categories': category_list
+    }
     return render(request, 'rango/index.html', context=context_dict)
+
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        # Try find a category with the slug given
+        category = Category.objects.get(slug=category_name_slug)
+
+        # Get associated pages
+        pages = Page.objects.filter(category=category)
+
+        # Update context
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        # Don't do anything, template will display no category message
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
 
 
 def about(request):
